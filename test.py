9591b45ddec652c5ae827d566d56e2f5b781d699
minhdc <cristiando0902@gmail.com>
2020-04-21 09:49:54 +0700

    add write text

--- test.py
+++ test.py
@@ -2,56 +2,68 @@ import pickle
 import numpy as np
 import cv2
 import os
+import tqdm
+import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--gt', type=str, default='./')
-parser.add_argument('--output', type=str, default='./image_test')
-parser.add_argument('--number_test', type=int, default=2)
+parser.add_argument('--output', type=str, default='./image_test/all_result')
+parser.add_argument('--number_test', type=int, default=-1)
 FLAGS = parser.parse_args()
 
 g_truth = FLAGS.gt
 
+def check_exists(path):
+	try:
+		if not os.path.exists(FLAGS.output):
+			os.makedirs(FLAGS.output)
+	except Exception as e:
+		print(e)
+		pass
+
+
 with open('gt.pkl', 'rb') as pkl_file:
         datas = pickle.load(pkl_file)
         #
-path = 'images'
-imgs = data[:FLAGS.number_test]
-for data in imgs:
+path_save_chars = os.path.join(FLAGS.output, "chars")
+path_save_words = os.path.join(FLAGS.output, "words")
+path_save_text = os.path.join(FLAGS.output, "text")
+#
+check_exists(path_save_chars)
+check_exists(path_save_words)
+check_exists(path_save_text)
+
+
+path = 'data/images'
+imgs = datas[:FLAGS.number_test]
+
+for data in tqdm.tqdm(imgs):
 	image_name = data[0]
 	word_bboxs = data[1]
 	texts = data[2]
 	chars_bboxs = data[3]
-	#
-	print(">>>>> Print Words bboxs: ")
-	print(word_bboxs)
-	np_word = np.asarray(word_bboxs)
-	print(">> shape words bboxs: ", np_word.shape)
-	print()
-	print("all texts: ", texts)
-	print()
-	print(">>>> print Chars bboxs")
-	print(chars_bboxs)
-	np_chars = np.asarray(chars_bboxs)
-	print(">> shape chars bboxs: ", np_chars.shape)
-	#
+
+	# print("all texts: ", texts)
+
 	img = cv2.imread(os.path.join(path, image_name))
-	print()
-	print(">>> Draw words bboxs: ", image_name)
 	_n = image_name.split('.')[0]
+	with open("image_test/all_result/text/{}.txt".format(_n), 'w') as f:
+		for word_text in texts:
+			f.write(word_text)
+			f.write("\n")
+
 	word_image = img.copy()
 	for bb in word_bboxs:
 		top, _, down, _ = bb
 		x, y, xx, yy = int(top[0]), int(top[1]), int(down[0]), int(down[1])
 		word_rect = cv2.rectangle(word_image, (x, y), (xx, yy), (255, 0, 255), 2)
-	cv2.imwrite('{}/word_rect_{}.jpg'.format(FLAGS.output, _n), word_rect)
+	cv2.imwrite('{}/words/word_rect_{}.jpg'.format(FLAGS.output, _n), word_rect)
 	#
-	print()
-	print(">>> Draw chars bboxs: ", image_name)
 
 	char_image = img.copy()
 	for char in chars_bboxs:
 		left_above, _, right_bottom, _ = char
-		print(">> char: ", char)
+		# print(">> char: ", char)
 		rect = cv2.rectangle(char_image,(int(left_above[0]), int(left_above[1])),\
 						(int(right_bottom[0]), int(right_bottom[1])), (255,0,255), 2)
-	cv2.imwrite('{}/char_rect_{}.jpg'.format(FLAGS.output, _n), rect)
+	cv2.imwrite('{}/chars/char_rect_{}.jpg'.format(FLAGS.output, _n), rect)
